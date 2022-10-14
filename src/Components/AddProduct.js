8a15062b44e28e7cc007c50875f63563062e9d39
DHINESH KUMAR DEVAMANI <45681349+DhineshKumar1989@users.noreply.github.com>
2022-10-14 09:37:13 +0530

    Initial commit

---
+++ src/Components/AddProduct.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export default function AddProduct(props) {
+
+    // Write Your Code Here
+
+    function handleProductNameChange(e) {
+        // Write Your Code Here
+    }
+
+    function handleBrandChange(e) {
+        // Write Your Code Here
+    }
+
+    function handleQuantityChange(e) {
+        // Write Your Code Here
+    }
+
+    function handlePriceChange(e) {
+        // Write Your Code Here
+    }
+
+    function handleFormSubmit(e) {
+        // Write Your Code Here
+    }
+
+    return (
+        <div>
+            {/* Write Your Code Here */}
+        </div>
+    )
+}
